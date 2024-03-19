9b4efcb312dcdc07f945dec28f39666600e0050f
Kasper Høglund <kasper@rubynor.com>
2024-03-19 15:54:44 +0100

    Add missing relations

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,5 @@ class Client < ApplicationRecord
   validates :description, presence: true, length: { minimum: 2, maximum: 100 }
 
   has_many :projects
+  has_many :time_regs, through: :projects
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   validates :name, format: { without: /\r|\n/, message: "Line breaks are not allowed" }
   validates :name, presence: :true, length: { minimum: 2, maximum: 50 }, uniqueness: true
   has_many :assigned_tasks
+  has_many :time_regs, through: :assigned_tasks
+  has_many :projects, through: :assigned_tasks
 end
