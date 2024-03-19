27ae83eb54ad10973b49fbc1ccc1c0f7c2724a2d
WildCard-NO <kalex.boe@gmail.com>
2024-03-19 14:48:06 +0100

    replaced sentry key with .env

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
-  config.dsn = "https://274b343b6ec0445dae647ef9794442f6@sentry.rubynor.com/16"
+  binding.pry
+  config.dsn = ENV["SENTRY_KEY"]
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   # Set traces_sample_rate to 1.0 to capture 100%
