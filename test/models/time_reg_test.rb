4887ea2a4638a8b5a307e4d029c8c56677130d0b
Kasper Høglund <kasper@rubynor.com>
2024-03-20 11:41:53 +0100

    Fix test

--- test/models/time_reg_test.rb
+++ test/models/time_reg_test.rb
@@ -9,7 +9,7 @@ class TimeRegTest < ActiveSupport::TestCase
 
     regs = TimeReg.for_report(clients, projects, users, tasks)
 
-    assert_equal 1, regs.count
-    assert_equal [ time_regs(:time_reg_1) ], regs.to_a
+    assert_equal 2, regs.count
+    assert_equal [ time_regs(:time_reg_1), time_regs(:time_reg_5) ], regs.to_a
   end
 end
