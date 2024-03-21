40c165d720484ed10a5f4e25fcb3a70ac3fe622b
Kasper Høglund <kasper@rubynor.com>
2024-03-21 14:15:48 +0100

    Add github action workflow

--- test/models/time_reg_test.rb
+++ test/models/time_reg_test.rb
@@ -10,6 +10,6 @@ class TimeRegTest < ActiveSupport::TestCase
     regs = TimeReg.for_report(clients, projects, users, tasks)
 
     assert_equal 2, regs.count
-    assert_equal [ time_regs(:time_reg_1), time_regs(:time_reg_5) ], regs.to_a
+    assert_equal [ time_regs(:time_reg_1), time_regs(:time_reg_5) ].sort, regs.to_a.sort
   end
 end
