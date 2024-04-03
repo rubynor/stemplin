44f05769879a930845e92b35e9ebdfdb619b21c9
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2024-04-03 10:30:37 +0200

    fix: datepicker updating datetime

--- app/javascript/controllers/custom_calendar_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-    setValue(value) {
-        this.element.value = value;
-
-    //     trying to mimic phlex ui input controller that is passed as an outlet;
-    }
-}
