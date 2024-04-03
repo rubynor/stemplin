2c61d52ed0c6bc2f9c4c0ff377c38403b4f2ac51
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2024-04-03 10:30:37 +0200

    fix: datepicker updating datetime

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -55,7 +55,4 @@ application.register("custom-dialog", DialogController);
 import SnackbarController from "./snackbar_controller";
 application.register("snackbar", SnackbarController);
 
-import CustomCalendarController from "./custom_calendar_controller";
-application.register("custom-calendar", CustomCalendarController);
-
 // TODO: fetch controllers from app/javascript/controllers/**/*_controller.js and load/register everything automatically, the filename of the controller will then be used as the controller identifier in the DOM
