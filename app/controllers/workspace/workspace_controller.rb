1958abb6cc854e902c686c03666adf40d5a83934
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2024-04-15 10:27:33 +0200

    Adding workspace action policies (#83)
    * chore: adding workspace action policies
    
    * chore: adding workspace action policies

--- app/controllers/workspace/workspace_controller.rb
+++ app/controllers/workspace/workspace_controller.rb
@@ -1,7 +1,13 @@
 module Workspace
   class WorkspaceController < ApplicationController
+    before_action :authorize!
+
     layout -> do
       "workspace"
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |ex|
+      redirect_to root_path
     end
 
     def index
