50bb20c107b115c33642c813967c7e86f6a60a29
Kasper Høglund <kasper@rubynor.com>
2024-04-19 17:35:34 +0200

    Fix workspace team page

--- app/controllers/workspace/teams_controller.rb
+++ app/controllers/workspace/teams_controller.rb
@@ -3,5 +3,9 @@ module Workspace
     def index
       # TODO: Return all users affiliated with the organization in their respective projects
     end
+
+    def implicit_authorization_target
+      User
+    end
   end
 end
