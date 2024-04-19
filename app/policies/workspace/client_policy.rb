6f0dd0deae2d47097b3d7b22912f68e2c06d4865
Kasper Høglund <kasper@rubynor.com>
2024-04-19 16:19:46 +0200

    Fix lint

--- app/policies/workspace/client_policy.rb
+++ app/policies/workspace/client_policy.rb
@@ -1,6 +1,5 @@
 module Workspace
   class ClientPolicy < WorkspacePolicy
-
     scope_for :relation do |relation|
       if user.organization_admin?
         relation.where(organization: user.current_organization)
