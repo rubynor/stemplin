47b379a1a467b77f5a8a426ac15a53c190150d92
Kasper Høglund <kasper@rubynor.com>
2024-04-19 18:21:40 +0200

    Fix workspace user page

--- app/policies/workspace/user_policy.rb
+++ app/policies/workspace/user_policy.rb
@@ -1,4 +1,11 @@
 module Workspace
   class UserPolicy < WorkspacePolicy
+    scope_for :relation do |relation|
+      if user.organization_admin?
+        relation.joins(:organizations).where(organizations: user.current_organization).distinct
+      else
+        relation.none
+      end
+    end
   end
 end
