3d77da176b2e2a1e006dcf05fc133a10280e0ca1
Kasper Høglund <kasper@rubynor.com>
2024-04-22 16:07:36 +0200

    Add modal for switching organization

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,19 @@
+class OrganizationsController < ApplicationController
+  def set_current_organization
+    @organization = Organization.find(params[:id])
+    authorize! @organization
+
+    ActiveRecord::Base.transaction do
+      access_infos = current_user.access_infos
+      active_access_info = current_user.access_info
+
+      begin
+        active_access_info.update(active: false)
+        access_infos.find_by(organization: @organization).update(active: true)
+      rescue ActiveRecord::StatementInvalid
+      end
+    end
+
+    redirect_to root_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,30 +24,13 @@ class User < ApplicationRecord
     access_info.organization
   end
 
-  def set_organization(organization)
-    return false unless access_infos.exists?(organization: organization)
-
-    ActiveRecord::Base.transaction do
-      begin
-        access_info.update(active: false)
-        access_infos.find_by(organization: organization).update(active: true)
-      rescue ActiveRecord::StatementInvalid
-        return false
-      end
-    end
-
-    true
-  end
-
-  private
-
   def access_info
     if access_infos.exists?(active: true)
-      access_infos.find_by(active: true)
+      access_info = access_infos.find_by(active: true)
     else
       access_info = access_infos.first
       access_info.update(active: true)
-      access_info
     end
+    access_info
   end
 end
