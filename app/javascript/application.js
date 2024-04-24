0b778ce2a93919d73c86d1680b9a3be21c1f3250
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2024-04-24 09:56:44 +0200

    feat: adding turbo confirm styled component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -21,3 +21,23 @@ StreamActions.remove_modal = function() {
         document?.body?.classList?.remove("overflow-hidden");
     }
 }
+
+Turbo.setConfirmMethod((message, element) => {
+    const dialog = document.getElementById("turbo-confirm-dialog");
+
+    dialog.querySelector("p").innerHTML = message;
+    dialog.showModal();
+    document?.body?.classList.add('overflow-hidden');
+
+    const confirmButton = dialog.querySelector('button[value="confirm"]');
+    console.log("---->element", element);
+    console.log("---->element.dataset", element.dataset);
+    confirmButton.disabled = element.dataset.turboConfirmDisabled === "true";
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            document?.body?.classList.remove("overflow-hidden");
+            resolve(dialog.returnValue === "confirm");
+        }, { once: true })
+    });
+});
