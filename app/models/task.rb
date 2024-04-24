1b92348777fc0560a7bbed11ea45546813e3ffd1
Kasper Høglund <kasper@rubynor.com>
2024-04-24 10:17:24 +0200

    Update task name validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   validates :name, format: { without: /\r|\n/, message: "Line breaks are not allowed" }
-  validates :name, presence: :true, length: { minimum: 2, maximum: 50 }, uniqueness: true
+  validates :name, presence: :true, length: { minimum: 2, maximum: 50 }, uniqueness: { scope: :organization }
+
   has_many :assigned_tasks
   has_many :time_regs, through: :assigned_tasks
   has_many :projects, through: :assigned_tasks
