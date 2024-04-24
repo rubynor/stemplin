d5deaa8ac72d53f165cb670e825673464ec7ab12
WildCard-NO <kalex.boe@gmail.com>
2024-04-24 10:25:46 +0200

    Update month and days translation

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Reap
     config.autoload_paths << "#{root}/app/views/components"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :nb]
 
     # Configuration for the application, engines, and railties goes here.
     #
