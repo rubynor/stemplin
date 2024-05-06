c2ac95ef75a89d0af4270953e73db6bc3d9727fa
WildCard-NO <kalex.boe@gmail.com>
2024-05-06 14:41:25 +0200

    Remove minimum length for description

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ApplicationRecord
   validates :name, format: { without: /\r|\n/, message: "Line breaks are not allowed" }
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }, uniqueness: { scope: :organization }
-  validates :description, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :description, length: { maximum: 100 }
 
   has_many :projects
   has_many :time_regs, through: :projects
