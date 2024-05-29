2fe3f1202e3513cf3fe57005c81e229c4c581683
Kasper Høglund <kasper@rubynor.com>
2024-05-29 09:01:16 +0200

    Refactor pagination_component

--- app/views/components/pagination_component.rb
+++ app/views/components/pagination_component.rb
@@ -10,28 +10,56 @@ class PaginationComponent < ApplicationComponent
   def template(&content)
     render PhlexUI::Pagination.new(class: "py-4") do
       render PhlexUI::Pagination::Content.new(class: "text-primary") do
-        render PhlexUI::Pagination::Item.new(href: first_path, class: default_class) do
-          content_tag(:i, "&#xe81a;".html_safe, class: "uc-icon text-xl")
-        end
-        render PhlexUI::Pagination::Item.new(href: prev_path, class: default_class) do
-          content_tag(:i, "&#xe81e;".html_safe, class: "uc-icon text-xl")
-        end
+        render_link_to_first
+        render_link_back_one
 
-        render PhlexUI::Pagination::Item.new(href: prev_path, class: default_class) { @pagy.prev } if @pagy.prev
-        render PhlexUI::Pagination::Item.new(href: current_path, active: true, class: active_class) { @pagy.page }
-        render PhlexUI::Pagination::Item.new(href: next_path, class: default_class) { @pagy.next } if @pagy.next
+        render_link_to_prev
+        render_link_to_current
+        render_link_to_next
 
-        render PhlexUI::Pagination::Item.new(href: next_path, class: default_class) do
-          content_tag(:i, "&#xe820;".html_safe, class: "uc-icon text-xl")
-        end
-        render PhlexUI::Pagination::Item.new(href: last_path, class: default_class) do
-          content_tag(:i, "&#xe81b;".html_safe, class: "uc-icon text-xl")
-        end
+        render_link_forward
+        render_link_to_last
       end
     end
   end
 
   private
+
+  def render_link_to_first
+    render PhlexUI::Pagination::Item.new(href: page_path(1), class: default_class) do
+      content_tag(:i, "&#xe81a;".html_safe, class: "uc-icon text-xl")
+    end
+  end
+
+  def render_link_back_one
+    render PhlexUI::Pagination::Item.new(href: page_path(@pagy.prev), class: default_class) do
+      content_tag(:i, "&#xe81e;".html_safe, class: "uc-icon text-xl")
+    end
+  end
+
+  def render_link_to_prev
+    render PhlexUI::Pagination::Item.new(href: page_path(@pagy.prev), class: default_class) { @pagy.prev } if @pagy.prev
+  end
+
+  def render_link_to_current
+    render PhlexUI::Pagination::Item.new(href: page_path(@pagy.page), active: true, class: active_class) { @pagy.page }
+  end
+
+  def render_link_to_next
+    render PhlexUI::Pagination::Item.new(href: page_path(@pagy.next), class: default_class) { @pagy.next } if @pagy.next
+  end
+
+  def render_link_forward
+    render PhlexUI::Pagination::Item.new(href: page_path(@pagy.next), class: default_class) do
+      content_tag(:i, "&#xe820;".html_safe, class: "uc-icon text-xl")
+    end
+  end
+
+  def render_link_to_last
+    render PhlexUI::Pagination::Item.new(href: page_path(@pagy.last), class: default_class) do
+      content_tag(:i, "&#xe81b;".html_safe, class: "uc-icon text-xl")
+    end
+  end
 
   def active_class
     "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground"
@@ -39,26 +67,6 @@ class PaginationComponent < ApplicationComponent
 
   def default_class
     "hover:bg-gray-100 hover:text-primary"
-  end
-
-  def current_path
-    page_path(@pagy.page)
-  end
-
-  def prev_path
-    page_path(@pagy.prev)
-  end
-
-  def next_path
-    page_path(@pagy.next)
-  end
-
-  def first_path
-    page_path(1)
-  end
-
-  def last_path
-    page_path(@pagy.last)
   end
 
   def page_path(page_number)
