17b8fb7ba5c4a0c73142826fdb23250be7f93535
Kasper Høglund <kasper@rubynor.com>
2024-06-18 09:44:34 +0200

    Typo

--- app/views/components/dropdown_component.rb
+++ app/views/components/dropdown_component.rb
@@ -66,6 +66,6 @@ class DropdownComponent::Content < ApplicationComponent
   end
 
   def default_classes
-    "z-50 rounded-md border bg-background p-2 text-foreground shadow-md outline-none mt-2 hidden left-0 custom-dropdown-component-content cursor-defaut"
+    "z-50 rounded-md border bg-background p-2 text-foreground shadow-md outline-none mt-2 hidden left-0 custom-dropdown-component-content cursor-default"
   end
 end
