0847db580dc9c1044dce31adf64b2c614d6d199e
Kasper Høglund <kasper@rubynor.com>
2024-06-19 09:32:45 +0200

    Update dropdown to fit screen

--- app/javascript/controllers/custom_dropdown_controller.js
+++ app/javascript/controllers/custom_dropdown_controller.js
@@ -4,6 +4,32 @@ export default class extends Controller {
   static targets = ['trigger', 'content'];
   static values = { closeBackgroundDelay: Boolean };
 
+  triggerTargetConnected() {
+    this.doStuff()
+    window.addEventListener('resize', () => this.doStuff());
+  }
+
+  doStuff() {
+    this.screenHeight = screen.height
+    this.originalRect = this.triggerTarget.getBoundingClientRect();
+    this.triggerTop = this.originalRect.bottom;
+
+    if (!this.originalRect) {
+      this.originalRect = this.triggerTarget.getBoundingClientRect();
+      this.triggerTop = this.originalRect.bottom;
+    }
+
+    const availableContentSpace = this.screenHeight - this.triggerTop - 10;
+
+    const contentRect = this.contentTarget.getBoundingClientRect();
+
+    if (contentRect.height > availableContentSpace) {
+      this.contentTarget.style.bottom = -Math.abs(availableContentSpace) + "px";
+    } else {
+      this.contentTarget.style.bottom = "unset";
+    }
+  }
+
   toggleContent(isActionOpen = true) {
     this.closeOtherDropdowns();
     this.contentTarget.classList.toggle('hidden', !isActionOpen);
