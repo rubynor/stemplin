b94234bd4781f7f2bc989f350008cab35a8d7e65
Kasper Høglund <kasper@rubynor.com>
2024-06-26 16:43:47 +0200

    User create not new

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,7 +21,7 @@ class Project < ApplicationRecord
 
     assigned_tasks.where(task_id: task_ids_to_remove, is_archived: false).update_all(is_archived: true)
     task_ids_to_add.each do |id_to_add|
-      assigned_tasks.new(task_id: id_to_add).save
+      assigned_tasks.create(task_id: id_to_add)
     end
   end
 end
