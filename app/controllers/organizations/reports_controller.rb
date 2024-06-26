a8e916fb5e712da005199d37c10e5e9c547556b2
Kasper Høglund <kasper@rubynor.com>
2024-06-26 15:31:42 +0200

    Add detailed report to organization report

--- app/controllers/organizations/reports_controller.rb
+++ app/controllers/organizations/reports_controller.rb
@@ -12,6 +12,33 @@ module Organizations
       @results = Organizations::Reports::Result.new(time_regs: @time_regs, filter: @filter)
     end
 
+    def detailed
+      @filter = Organizations::Reports::Filter.new(filter_params)
+
+      time_regs = authorized_scope(TimeReg, type: :relation).between_dates(@filter.start_date, @filter.end_date)
+
+      time_regs = filtered_collection(time_regs)
+      total_billable_minutes = time_regs.joins(:project).where(project: { billable: true }).sum(&:minutes)
+      total_minutes = time_regs.sum(&:minutes)
+      clients = authorized_scope(Client, type: :relation).joins(:time_regs).where(time_regs: { id: time_regs }).distinct
+      projects = authorized_scope(Project, type: :relation).joins(:time_regs).where(time_regs: { id: time_regs }).distinct
+      tasks = authorized_scope(Task, type: :relation).joins(:time_regs).where(time_regs: { id: time_regs }).distinct
+      users = authorized_scope(User, type: :relation).joins(:time_regs).where(time_regs: { id: time_regs }).distinct
+
+      @structured_report_data = time_regs.group_by { |reg| reg.date_worked }.sort_by { |key| key  }
+      @detailed_report_data = OpenStruct.new(
+        time_regs: time_regs,
+        total_billable_minutes: total_billable_minutes,
+        total_minutes: total_minutes,
+        clients: clients,
+        projects: projects,
+        tasks: tasks,
+        users: users,
+      )
+
+      # render "organizations/reports/detailed/show"
+    end
+
     private
 
     def filter_params
--- app/policies/organizations/report_policy.rb
+++ app/policies/organizations/report_policy.rb
@@ -1,6 +1,6 @@
 module Organizations
   class ReportPolicy < ApplicationPolicy
-    [ :index? ].each do |action|
+    [ :index?, :detailed? ].each do |action|
       define_method(action) do
         user.organization_admin?
       end
