c5b163c36fedfe395de5e14abbdcd06723029bf6
Kasper Høglund <kasper@rubynor.com>
2024-06-27 13:13:46 +0200

    Inscease bottom margin

--- app/javascript/controllers/custom_dropdown_controller.js
+++ app/javascript/controllers/custom_dropdown_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
     }
 
     let triggerBottom = this.triggerTarget.getBoundingClientRect().bottom;
-    const availableContentSpace = window.innerHeight - triggerBottom - 10;
+    const availableContentSpace = window.innerHeight - triggerBottom - 20;
 
     if (this.originalContentHeight > availableContentSpace) {
       this.contentTarget.style.bottom = -availableContentSpace + "px";
