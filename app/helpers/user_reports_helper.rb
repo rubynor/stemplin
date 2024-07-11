95086e0c6952ea638ea7e3fed41992f1e5bebf19
Mathias Presthagen <mathias.presthagen@revolve.no>
2024-07-11 13:38:44 +0200

    Removed files and methods belonging to deprecated models UserReport, ProjectReport and Membership

--- app/helpers/user_reports_helper.rb
+++
@@ -1,3 +0,0 @@
-module UserReportsHelper
-  include ReportsHelper
-end
--- test/controllers/project_report_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ProjectReportControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
