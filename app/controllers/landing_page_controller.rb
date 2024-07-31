616b8f5b52fc8426b35601f9f1b2777640ae3a85
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2024-07-31 07:05:06 +0200

    feat: landing page - testimonials

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,10 +1,13 @@
 class LandingPageController < ApplicationController
   def index
     @services = services
+    @testimonials = testimonials
+    @getting_started_steps = getting_started_steps
   end
 
   private
 
+  # @Note: Keeping these static landing pages content until i find a better place
   def services
     [
       {
@@ -14,23 +17,61 @@ class LandingPageController < ApplicationController
         illustration: "easy-time-logging.svg"
       },
       {
-        title: "Full visibility",
-        identifier: "full-visibility",
-        description: "Detailed insight into team activities and durations across all projects, providing comprehensive reports that highlight individual contributions and overall team performance.",
+        title: "Easy billing",
+        identifier: "easy-billing",
+        description: "Managers can easily send invoices to clients based on registered time, and include comprehensive time reports, ensuring transparency and customer satisfaction.",
         illustration: "full-visibility.svg"
       },
       {
         title: "Workspace administration",
         identifier: "workspace-administration",
-        description: "Easily manage and onboard team members within your workspace, ensuring seamless integration and effective collaboration on all tasks.",
+        description: "Easily manage and onboard team members with your workspace, ensuring seamless and effective collaboration on all tasks.",
         illustration: "workspace-administration.svg"
       },
       {
-        title: "Performance tracking",
-        identifier: "performance-tracking",
-        description: "Monitor productivity and identify areas for efficiency improvement, leveraging data analytics to pinpoint bottlenecks and optimize workflow processes for better outcomes.",
+        title: "Comprehensive insights",
+        identifier: "comprehensive-insights",
+        description: "Time tracking provides managers with detailed insights into activities and time allocations across all projects. The data is presented in comprehensive reports that can be leveraged to improve efficiency and productivity.",
         illustration: "performance-tracking.svg"
       }
     ]
+  end
+
+  # @Note: Keeping these static landing pages content until i find a better place
+  def testimonials
+    [
+      {
+        first_name: "John",
+        last_name: "Doe",
+        quote: lorem_ipsum,
+        role: "Devops Engineer"
+      },
+      {
+        first_name: "Marty",
+        last_name: "Harper",
+        quote: lorem_ipsum,
+        role: "QA Engineer"
+      },
+      {
+        first_name: "Robert",
+        last_name: "Henn",
+        quote: lorem_ipsum,
+        role: "Product Owner"
+      },
+      {
+        first_name: "Jack",
+        last_name: "Doe",
+        quote: lorem_ipsum,
+        role: "Devops Engineer"
+      }
+    ]
+  end
+
+  def lorem_ipsum
+    "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."
+  end
+
+  def getting_started_steps
+    [ "Sign up", "Create a project", "Invite your team", "Start tracking time" ]
   end
 end
