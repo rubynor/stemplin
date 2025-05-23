f0aee74813a37e6e69c947327d78f9773d67f6f0
Kasper Høglund <kasper@rubynor.com>
2025-05-23 15:24:43 +0200

    Lint

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Stemplin
     config.exceptions_app = self.routes
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
