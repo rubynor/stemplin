f3553ff6672f910ceebe00e8ebc292044c4c932b
fogadOg <fogadm06@gmail.com>
2025-05-30 15:57:43 +0200

    Breadcrumb

---
+++ app/components/ruby_ui/breadcrumb/breadcrumb_link.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module RubyUI
+  class BreadcrumbLink < Base
+    def initialize(href: "#", **attrs)
+      @href = href
+      super(**attrs)
+    end
+
+    def view_template(&)
+      a(href: @href, **attrs, &)
+    end
+
+    private
+
+    def default_attrs
+      {
+        class: "whitespace-nowrap inline-flex items-center justify-center rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary-600 underline-offset-4 hover:underline",
+      }
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -122,7 +122,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_21_094434) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
-    t.boolean "is_verified", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
