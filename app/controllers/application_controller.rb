7565964412034a674cbdfb2ae07e769d29a10af1
Kasper Høglund <kasper@rubynor.com>
2025-06-17 16:24:23 +0200

    Refactor redirect methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
-    allowed_controllers = %w[onboarding_wizard invitations service_worker sessions]
-    onboarding_wizard_path(:organization) if current_user&.organizations&.empty? && !allowed_controllers.include?(controller_name)
+    return onboarding_wizard_path(:organization) if should_redirect_to_onboarding?
     root_path
   end
 
@@ -38,7 +37,11 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_if_no_organization
+    redirect_to onboarding_wizard_path(:organization) if should_redirect_to_onboarding?
+  end
+
+  def should_redirect_to_onboarding?
     allowed_controllers = %w[onboarding_wizard invitations service_worker sessions]
-    redirect_to onboarding_wizard_path(:organization) if current_user&.organizations&.empty? && !allowed_controllers.include?(controller_name)
+    current_user&.organizations&.empty? && !allowed_controllers.include?(controller_name)
   end
 end
